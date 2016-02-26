72c5dfd6c47dd0f77b80741b31329fa484460565
Joey T <thechickenbane@gmail.com>
2016-02-26 11:21:55 -0800

    16

--- src/main/kotlin/advent/Day15.kt
+++ src/main/kotlin/advent/Day15.kt
@@ -152,11 +152,7 @@ Given the ingredients in your kitchen and their properties, what is the total sc
         }
 
 
-        override fun hasNext(): Boolean {
-            if (bars.isEmpty()) return true
-            return bars.first() < bucketMax
-        }
-
+        override fun hasNext(): Boolean = if (bars.isEmpty()) true else bars.first() < bucketMax
     }
 
     fun answer(): Long {
@@ -172,5 +168,36 @@ Given the ingredients in your kitchen and their properties, what is the total sc
         return max
     }
 
+    private val part2 = """
+    --- Part Two ---
 
+Your cookie recipe becomes wildly popular! Someone asks if you can make another recipe that has exactly 500 calories per cookie (so they can use it as a meal replacement). Keep the rest of your award-winning process the same (100 teaspoons, same ingredients, same scoring system).
+
+For example, given the ingredients above, if you had instead selected 40 teaspoons of butterscotch and 60 teaspoons of cinnamon (which still adds to 100), the total calorie count would be 40*8 + 60*3 = 500. The total score would go down, though: only 57600000, the best you can do in such trying circumstances.
+
+Given the ingredients in your kitchen and their properties, what is the total score of the highest-scoring cookie you can make with a calorie total of 500?
+    """
+
+    fun answer2(): Long {
+        var max = 0L
+        val spoons = ComboIterator(TotalSpoons, puzzleIngredients.size)
+        for (arr in spoons) {
+            val score = cookieScore(puzzleIngredients, arr)
+            val calories = calories(puzzleIngredients, arr)
+            if (score > max && calories == 500) {
+                println("new score = $max old score = $score")
+                max = score
+            }
+        }
+        return max
+    }
+
+    fun calories(ingredients: List<Ingredient>, spoons: IntArray): Int {
+        var calories = 0
+        for ((idx, ing) in ingredients.withIndex()) {
+            val ingSpoons = spoons[idx]
+            calories += ingSpoons * ing.calories
+        }
+        return calories
+    }
 }
