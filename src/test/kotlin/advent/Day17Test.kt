b0d477f24c7093865e9fe692ae9332d899c4b8f6
Joey T <thechickenbane@gmail.com>
2016-03-07 23:28:26 -0800

    17 complete

--- src/test/kotlin/advent/Day17Test.kt
+++ src/test/kotlin/advent/Day17Test.kt
@@ -3,16 +3,20 @@ package advent
 
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 
 /**
  * Created by joeyt on 3/1/16.
  */
 class Day17Test {
+
     @Test
     fun example() {
         val ex = listOf(20, 15, 10, 5, 5).map { it.toString() }
         val bins = Day17.input2Bins(ex)
-        Assert.assertEquals("example had 4 solutions", 4, Day17.findNumCombos(bins, 25))
+        val combos = HashSet<List<Int>>()
+        Day17.populateCombos(LinkedList(), bins.toCollection(LinkedList()), 25, combos)
+        Assert.assertEquals("example had 4 solutions", 4, combos.size)
     }
 
     @Test
@@ -20,4 +24,20 @@ class Day17Test {
         Assert.assertEquals("my answer", 4372, Day17.answer())
     }
 
+    @Test
+    fun example2() {
+        val ex = listOf(20, 15, 10, 5, 5).map { it.toString() }
+        val bins = Day17.input2Bins(ex)
+        val combos = HashSet<List<Int>>()
+        Day17.populateCombos(LinkedList(), bins.toCollection(LinkedList()), 25, combos)
+        val groupBy: Map<Int, List<List<Int>>> = combos.groupBy { it.size }
+        val minCombos = groupBy.keys.min()!!
+        val num = groupBy[minCombos]!!.size
+        Assert.assertEquals("example had 3 ways", 3, num)
+    }
+
+    @Test
+    fun answer2() {
+        Assert.assertEquals("my answer", 4, Day17.answer2())
+    }
 }
